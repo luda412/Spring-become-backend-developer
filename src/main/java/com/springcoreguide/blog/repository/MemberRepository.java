5a59fa185742a7f64179aee14a82db59ae792254
luda412 <201810791@sangmyung.kr>
2025-03-24 12:40:11 +1030

    [Feat] - findByName method

--- src/main/java/com/springcoreguide/blog/repository/MemberRepository.java
+++ src/main/java/com/springcoreguide/blog/repository/MemberRepository.java
@@ -1,9 +1,13 @@
 package com.springcoreguide.blog.repository;
 
-import com.springcoreguide.blog.domain.Member;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.springcoreguide.blog.domain.Member;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long>{
+    Optional<Member> findByName(String name);
 }
